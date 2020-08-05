1022199cf3ed8db27fcf6c8d1d9d728951498e85
Hemanth <hemanth.kotagiri43@gmail.com>
2020-08-06 00:55:09 +0530

    Complete ex5 - LRCF & Reg. Gradient

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -19,16 +19,20 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+%size(X)
+%size(y)
+%size(theta)
 
+J = sum(((X * theta) - y).^2);
+J = (1/(2*m))*J;
+reg = (lambda/(2*m)) * sum((theta(2:end)).^2);
 
+J = J + reg;
 
+thetaZero = theta;
+thetaZero(1) = 0;
 
-
-
-
-
-
-
+grad = ((1/(m)*(((X * theta) - y)' * X)) + (thetaZero'*(lambda/m)));
 
 % =========================================================================
 
