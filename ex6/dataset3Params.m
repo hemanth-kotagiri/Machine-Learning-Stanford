0c66c6370cbe6441872c3ab4ffac58610588b019
Hemanth <hemanth.kotagiri43@gmail.com>
2020-10-25 15:35:37 +0530

    Complete Hyperparameters(C, Sigma) opt. using loops

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,10 +23,25 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+all_models_errors = eye(64,3);
+i = 0;
+
+for C_new = [0.01 0.03 0.1 0.3 1 3 10 30]
+    for sigma_new = [0.01 0.03 0.1 0.3 1 3 10 30]
+        model = svmTrain(X, y, C_new, @(x1, x2) gaussianKernel(x1, x2, sigma_new));
+        preds = svmPredict(model, Xval);
+        curr_error = mean(double(preds ~= yval));
+
+        i = i + 1;
+        all_models_errors(i,:) = [C_new, sigma_new, curr_error];
+
+    end
+end
 
 
-
-
+sorted_errors = sortrows(all_models_errors, 3);
+C = sorted_errors(1,1);
+sigma = sorted_errors(1,2);
 
 
 % =========================================================================
